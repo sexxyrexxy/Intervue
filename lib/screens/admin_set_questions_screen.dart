dc16c5e7dc860946894b032d68568083bda7c493
sexxyrexxy <rexlim2003@gmail.com>
2023-10-27 15:44:57 +0800

    siu

---
+++ lib/screens/admin_set_questions_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
+
+class AdminSetInterviewScreen extends StatefulWidget {
+  const AdminSetInterviewScreen({super.key});
+
+  @override
+  State<AdminSetInterviewScreen> createState() => _AdminSetInterviewScreenState();
+}
+
+class _AdminSetInterviewScreenState extends State<AdminSetInterviewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        
+    );
+  }
+}

2cc629688cc5a179b91d91e2b26920f737bed2a5
sexxyrexxy <rexlim2003@gmail.com>
2023-10-29 23:35:04 +0800

    ok

--- lib/screens/speech_to_text.dart
+++ lib/screens/speech_to_text.dart
@@ -9,104 +9,95 @@ class SpeechToTextTest extends StatefulWidget {
   _SpeechToTextTest createState() => _SpeechToTextTest();
 }
 
-
 class _SpeechToTextTest extends State {
   final SpeechToText _speechToText = SpeechToText();
-  bool _speechEnabled = false;
-  String _lastWords = '';
-  String _currentWords = '';
+  bool isListening = false;
+  String text = '';
 
   @override
   void initState() {
     super.initState();
-    _initSpeech();
+    checkMicrophoneAvailability();
   }
 
   /// This has to happen only once per app
-  void _initSpeech() async {
-    _speechEnabled = await _speechToText.initialize(
-        onStatus: statusListener
-    );
-    setState(() {});
-  }
-
   /// Each time to start a speech recognition session
-  void _startListening() async {
-    await _speechToText.listen(onResult: _onSpeechResult, listenMode: ListenMode.dictation);
-    setState(() {});
-  }
-
-  /// Manually stop the active speech recognition session
-  /// Note that there are also timeouts that each platform enforces
-  /// and the SpeechToText plugin supports setting timeouts on the
-  /// listen method.
-  void _stopListening() async {
-    await _speechToText.stop();
-    setState(() {});
-  }
-
-  /// This is the callback that the SpeechToText plugin calls when
-  /// the platform returns recognized words.
-  void _onSpeechResult(SpeechRecognitionResult result) {
-    setState(() {
-      _lastWords = result.recognizedWords;
-    });
-  }
-
-  void statusListener(String status) async {
-    debugPrint("status $status");
-    if (status == "done" && _speechEnabled) {
+  void checkMicrophoneAvailability() async {
+    bool available = await _speechToText.initialize();
+    if (available) {
       setState(() {
-        _lastWords += " $_currentWords";
-        _currentWords = "";
-        _speechEnabled = false;
+        print('Microphone available: $available');
       });
-      _startListening();
+    } else {
+      print("The user has denied the use of speech recognition.");
+    }
+  }
+
+  Future<void> startListening() async {
+    if (!isListening) {
+      var available = await _speechToText.initialize();
+      if (available) {
+        setState(() {
+          isListening = true;
+        });
+        _speechToText.listen(
+          listenMode: ListenMode.dictation,
+            listenFor: const Duration(minutes: 3),
+            pauseFor: Duration(seconds: 10),
+            onResult: (result) {
+              setState(() {
+                text = result.recognizedWords;
+              });
+            });
+      }
+    } else {
+      setState(() {
+        isListening = false;
+      });
+      _speechToText.stop();
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
           children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
-              child: Text(
-                'Test Speech-To-Text',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w500,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ]
-        ),
-        Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+            const Row(mainAxisAlignment: MainAxisAlignment.center, children: [
               Padding(
                 padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
-                child: FloatingActionButton(
-                    onPressed: _speechToText.isNotListening ? _startListening : _stopListening,
-                    tooltip: 'Start/Stop',
-                    child: const Icon(Icons.mic)
+                child: Text(
+                  'Test Speech-To-Text',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
               ),
-            ]
-        ),
-        Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                child: GestureDetector(
+                    onTap: () {
+                      startListening();
+                    },
+                    child: const Icon(Icons.mic)),
+              ),
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
               Padding(
                 padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
                 child: SizedBox(
                   width: 600,
                   child: Text(
-                    _speechToText.isListening ? _lastWords : _speechEnabled ? 'Tap the microphone to start listening...' : 'Speech not available',
+                    _speechToText.isListening
+                        ? text
+                        : isListening
+                            ? 'Tap the microphone to start listening...'
+                            : 'Speech not available',
                     style: const TextStyle(
                       fontSize: 30,
                       fontWeight: FontWeight.w100,
@@ -115,9 +106,10 @@ class _SpeechToTextTest extends State {
                   ),
                 ),
               ),
-            ]
+            ]),
+          ],
         ),
-      ],
+      ),
     );
   }
 }

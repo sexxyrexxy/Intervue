9926a4ff73cdd84f7e901f4cde3ef5fbeeb5030e
bonifacioronald <bonifacioronald99@gmail.com>
2023-11-25 15:22:09 +0800

    feat: add position model

--- lib/models/position_model.dart
+++ lib/models/position_model.dart
@@ -1,11 +1,25 @@
 class PositionModel {
   String id;
   String name;
+  String description;
+  int numOfPeople;
+  String location;
+  int yearOfExperience;
+  List<String> responsibilities;
+  List<String> benefits;
+  List<String> skillsRequired;
   List<String> questions;
 
   PositionModel({
     required this.id,
     required this.name,
+    required this.description,
+    required this.numOfPeople,
+    required this.location,
+    required this.yearOfExperience,
+    required this.responsibilities,
+    required this.benefits,
+    required this.skillsRequired,
     required this.questions,
   });
 }

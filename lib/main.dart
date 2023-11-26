6bc605569b1a5b2868c36de3cf15289f5598f8bf
sexxyrexxy <rexlim2003@gmail.com>
2023-11-26 20:35:47 +0800

    can

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'Intervüe',
           theme: ThemeData(fontFamily: 'Futura'),
-          home: Navigation(),
+          home: CandidatesUploadCV(),
           routes: {
             AdminMainScreen.routeName: (context) => AdminMainScreen(),
             newJobPostionScreenII.routeName: (context) =>
